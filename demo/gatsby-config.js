dcb6432de99a3065feade136d0b4dccfb0d356e5
abumalick <abdullah.hilson@gmail.com>
2020-04-05 11:54:00 +0100

    feat: update demo to use last version of the script and sentry configuration

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -9,8 +9,14 @@ module.exports = {
       resolve: 'gatsby-plugin-load-script',
       options: {
         disable: !process.env.SENTRY_DSN,
-        src: 'https://browser.sentry-cdn.com/5.5.0/bundle.min.js',
-        onLoad: `() => Sentry.init({dsn:"${process.env.SENTRY_DSN}"})`,
+        src: 'https://browser.sentry-cdn.com/5.15.4/bundle.min.js',
+        crossorigin: 'anonymous',
+        integrity:
+          'sha384-Nrg+xiw+qRl3grVrxJtWazjeZmUwoSt0FAVsbthlJ5OMpx0G08bqIq3b/v0hPjhB',
+        onLoad: `() => {
+          console.log("Callback called");
+          Sentry.init({dsn:"${process.env.SENTRY_DSN}"})
+        }`,
       },
     },
 
